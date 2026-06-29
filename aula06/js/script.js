7f2ae1bd6f459fd395028872a823bdec9305acf1
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 20:22:01 -0300

    Atualização JavaScript

--- aula06/js/script.js
+++ aula06/js/script.js
@@ -2,10 +2,20 @@ function exponencial() {
     let bas = window.document.getElementById('ibase')
     let exp = window.document.getElementById('iexp')
     let res = window.document.getElementById('resposta')
+
+    // Verifica se os campos estão vazios
+    if (bas.value.trim() === "" || exp.value.trim() === "") {
+        res.innerHTML = "<p>Por favor, preencha a base e o expoente.</p>"
+        bas.focus()
+        return
+    }
+    
     let base = Number(bas.value)
     let expo = Number(exp.value)
     let funcaoExp = Math.pow(base, expo)
-    res.innerHTML = `Função Exponencial: ${base}<sup>${expo}</sup> <br> Resultado: <strong> ${funcaoExp}</strong>`
+    res.innerHTML = `<p> F ( x ) = ${base} <sup>${expo}</sup> 
+    <br> 
+    F ( x ) = <strong> ${funcaoExp}</strong> </p>`
 }
 
 // A função Math.pow() retorna a base elevada ao expoente
