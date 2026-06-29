451d4f6e9d1b7c181cdb4aa6cb4d9f2f45217624
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 20:50:24 -0300

    Atualização Estilo CSS. Atualização JavaScript

--- aula09/js/script.js
+++ aula09/js/script.js
@@ -2,11 +2,23 @@ function calcular() {
     let anoA = window.document.querySelector('#anoAt')
     let anoN = window.document.querySelector('#anoNasc')
     let res = document.getElementById('resposta')
+    
+     // Verifica se os campos estão vazios
+    if (anoA.value.trim() === "" || anoN.value.trim() === "") {
+        res.innerHTML = "<p>Por favor, preencha os Campos.</p>"
+        anoA.focus()
+        return
+    }
+    
     let anoAtual = Number(anoA.value)
     let anoNasc = Number(anoN.value)
-   let idade = anoAtual - anoNasc
+    let idade = anoAtual - anoNasc
     res.innerHTML = `Em ${anoAtual} você terá ${idade} anos.<br>`
-    if (idade >= 21) {
-        res.innerHTML = `Em ${anoAtual} você terá ${idade} anos.<br> Com esta idade já terá atingido a maioridade!`
+    if (idade >= 18) {
+        res.innerHTML = `<p>Em ${anoAtual} você terá ${idade} anos.<br>
+        Com esta idade já terá atingido a maioridade!</p>`
+    } else {
+        res.innerHTML = `<p>Em ${anoAtual} você terá ${idade} anos.<br>
+        Ainda não terá atingido a maioridade.</p>`
     }
 }
