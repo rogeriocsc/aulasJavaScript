a68141ed1bc2217f3a9afc981dee62b629d39b2d
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 20:17:41 -0300

    Atualização Estilo CSS. Atualização JavaScript

--- aula05/js/script.js
+++ aula05/js/script.js
@@ -2,8 +2,22 @@ function raizQ() {
     let raiz = window.document.getElementById('iraiz')
     let res = window.document.getElementById('resposta')
     let raizQ = Number(raiz.value)
+
+    // Verifica se o campo está vazio
+    if (raiz.value.trim() === "") {
+        alert("Digite um número!")
+        raiz.focus()
+        return
+    }
+
+     // Verifica se o número é negativo
+    if (raizQ < 0) {
+        alert("Não é possível calcular a raiz quadrada de um número negativo.") 
+        return
+    }
+
     let rQ = Math.sqrt(raizQ)
-    res.innerHTML = `<p>A raiz quadrada do número <strong> ${raizQ} </strong> é <strong> ${rQ} </strong> </p>`
+    res.innerHTML = `<p>A raiz quadrada do <br> número <strong> ${raizQ} </strong> é: <br> <strong> ${rQ} </strong> </p>`
 }
 
 // A função Math.sqrt() retorna a raiz quadrada de um número..
