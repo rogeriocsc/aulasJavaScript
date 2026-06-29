dd32070bb826be0117f4c459ec20d0f024c6abee
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 20:31:51 -0300

    Atualização Estilo CSS. Atualização JavaScript

--- aula07/js/script.js
+++ aula07/js/script.js
@@ -2,9 +2,19 @@ function media() {
     let val1 = document.querySelector('#ival1')
     let val2 = document.querySelector('#ival2')
     let res = document.querySelector('#resposta')
+
+    // Verifica se os campos estão vazios
+    if (val1.value.trim() === "" || val2.value.trim() === "") {
+        res.innerHTML = "<p>Por favor, preencha os campos de Valores.</p>"
+        val1.focus()
+        return
+    }
+
     let valor1 = Number(val1.value)
     let valor2 = Number(val2.value)
     let media = (valor1 + valor2) / 2
-    res.innerHTML = `A média entre os valores <strong> ${valor1} </strong> e <strong> 
-    ${valor2} </strong> corresponde a <strong> ${media.toFixed(2)} </strong>`
+    res.innerHTML = `<p>Valores: <strong> ${valor1.toFixed(2)} </strong> e <strong> ${valor2.toFixed(2)} </strong> 
+    <br> --------------------------- <br> 
+    Média: <strong> ${media.toFixed(2)} </strong> 
+    <br> --------------------------- <br> </p>`
 }
--- aula08/js/script.js
+++ aula08/js/script.js
@@ -2,42 +2,49 @@ let prod = window.document.getElementById('iprod')
 let prec = window.document.getElementById('ipreco')
 let oper = window.document.getElementById('operacao')
 let res = window.document.getElementById('resposta')
-let desconto = undefined
-let valorPromocional = undefined
 
 function verificar() {
-    if (prod.value.length == 0 || prec.value.length == 0 || oper.value.length == 0) {
-        window.alert('Informe os Campos!')
-        window.location.reload()
-    } 
+    if (prod.value.length == 0 ||
+        prec.value.length == 0 ||
+        oper.value.length == 0) {
+        alert('Informe os Campos!')
+        return true
+    } else {
+        return false
+    }
 }
-
 function calcular() {
-    verificar()
-    let produto = (prod.value)
+    if (verificar()) {
+        return
+    }
+
+    let produto = prod.value
     let preco = Number(prec.value)
-    let op = (oper.value)
+    let op = oper.value
+    let porcentagem = 0
     switch (op) {
         case 'A':
-            desconto = (preco * 5) / 100
-            valorPromocional = preco - desconto
+            porcentagem = 5
             break;
         case 'B':
-            desconto = (preco * 10) / 100
-            valorPromocional = preco - desconto
+            porcentagem = 10
             break;
         case 'C':
-            desconto = (preco * 15) / 100
-            valorPromocional = preco - desconto
+            porcentagem = 15
             break;
         case 'D':
-            desconto = (preco * 20) / 100
-            valorPromocional = preco - desconto
+            porcentagem = 20
             break;
         default:
             res.innerHTML = '[ERRO] Valor Inválido!'
-            break;
+            return
     }
-    res.innerHTML = `Produto: ${produto} <br> Valor: R$ ${preco.toFixed(2)} <br> Valor com Desconto: R$ ${valorPromocional.toFixed(2)}`
+
+    let desconto = preco * porcentagem / 100
+    let valorPromocional = preco - desconto
+
+    res.innerHTML = `<p>Produto: ${produto} <br> 
+    Preço: R$ ${preco.toFixed(2)} <br> 
+    Valor com Desconto: R$ ${valorPromocional.toFixed(2)}</p>`
 }
 
